2a00e1fbbd512a4b46ffda120cdebb2bcce40ed0
stfsy <s.pfaffel@gmail.com>
2016-07-31 17:39:40 +0200

    test(incrementer.spec.js): add test for meta data support

--- test/specs/incrementer.spec.js
+++ test/specs/incrementer.spec.js
@@ -9,16 +9,17 @@ const fileSystem = require('fs-promise')
 
 describe('Incrementer', () => {
 
-    let incrementer = null 
+    let incrementer = null
 
     beforeEach(() => {
 
         incrementer = new Incrementer([resolve('test/test.package.json')])
-    })        
+    })
 
     afterEach(() => {
 
-        fileSystem.writeJson(resolve('test/test.package.json'), {version: '1.2.3'})
+        fileSystem.writeJsonSync(resolve('test/test.package.json'), { version: '1.2.3' })
+        fileSystem.writeJsonSync(resolve('test/test.package.withBuildMeta.json'), { version: '1.4.15+beta14-ABC' })
     })
 
     it('should read the version from a given target .json file', (done) => {
@@ -30,18 +31,35 @@ describe('Incrementer', () => {
         }).then(done, done)
     })
 
-    it('should persist an increment version number', (done) => {
+    it('should persist an incremented version number', (done) => {
 
         incrementer.readVersion().then((version) => {
 
             version.major(true)
 
         }).then(incrementer.persistAll.bind(incrementer))
-          .then(incrementer.readVersion.bind(incrementer))
-          .then((version) => {
+            .then(incrementer.readVersion.bind(incrementer))
+            .then((version) => {
 
-            expect(version.toString()).to.equal('2.0.0')
+                expect(version.toString()).to.equal('2.0.0')
 
-        }).then(done, done)
+            }).then(done, done)
+    })
+
+    it('should persist an incremented version number without build number if no build function was given', (done) => {
+
+        incrementer = new Incrementer([resolve('test/test.package.withBuildMeta.json')])
+
+        incrementer.readVersion().then((version) => {
+
+            version.major(true).meta()
+
+        }).then(incrementer.persistAll.bind(incrementer))
+            .then(incrementer.readVersion.bind(incrementer))
+            .then((version) => {
+
+                expect(version.toString()).to.equal('2.0.0')
+
+            }).then(done, done)
     })
 })
