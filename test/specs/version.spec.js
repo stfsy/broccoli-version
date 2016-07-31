495808319843db7bb9c37981a73720bcdc4fa60c
stfsy <s.pfaffel@gmail.com>
2016-07-31 17:38:51 +0200

    test(version.spec.js): add tests for meta data support

--- test/specs/version.spec.js
+++ test/specs/version.spec.js
@@ -6,12 +6,14 @@ const expect = require('chai').expect
 describe('Version', () => {
 
     let semver = null
+    let versionWithBuildMeta = null
     let version = null
 
     beforeEach(() => {
 
         semver = '0.0.1'
         version = new Version(semver)
+        versionWithBuildMeta = new Version(semver + '+415-beta')
     })
 
     it('should parse a valid semver string', () => {
@@ -19,14 +21,49 @@ describe('Version', () => {
         expect(version.toString()).to.equal(semver)
     })
 
+    it('should parse a valid semver string with build metadata', () => {
+
+        expect(versionWithBuildMeta.toString()).to.equal(semver)
+    })
+
     it('should not increment the patch level', () => {
 
         expect(version.patch(undefined).toString()).to.equal('0.0.1')
     })
 
+    it('should increment the patch level and not append build data', () => {
+
+        expect(versionWithBuildMeta.patch(true).toString()).to.equal('0.0.2')
+    })
+
+    it('should call back with correct major, minor patch und meta data values', () => {
+
+        versionWithBuildMeta.meta((version) => {
+
+            expect(version.major).to.equal(0)
+            expect(version.minor).to.equal(0)
+            expect(version.patch).to.equal(1)
+            expect(version.meta).to.equal('415-beta')
+        })
+    })
+
+    it('should only not update any part of the version string', () => {
+
+        expect(versionWithBuildMeta.meta().toString()).to.equal('0.0.1')
+    })
+
+    it('should only update build meta data', () => {
+
+        expect(versionWithBuildMeta.meta(() => {
+
+            return 'beta413'
+
+        }).toString()).to.equal('0.0.1' + '+beta413')
+    })
+
     it('should increment the patch level', () => {
 
-        expect(version.patch().toString()).to.equal('0.0.2')
+        expect(version.patch(true).toString()).to.equal('0.0.2')
     })
 
     it('should increment the patch level', () => {
@@ -51,11 +88,6 @@ describe('Version', () => {
 
     it('should increment the minor level', () => {
 
-        expect(version.minor().toString()).to.equal('0.1.0')
-    })
-
-    it('should increment the minor level', () => {
-
         expect(version.minor(true).toString()).to.equal('0.1.0')
     })
 
@@ -72,11 +104,6 @@ describe('Version', () => {
     it('should not increment the major level', () => {
 
         expect(version.major(undefined).toString()).to.equal('0.0.1')
-    })
-
-    it('should increment the major level', () => {
-
-        expect(version.major().toString()).to.equal('1.0.0')
     })
 
     it('should increment the major level', () => {
