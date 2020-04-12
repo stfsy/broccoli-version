5f8b0df3510b5082afb38559bfd46626b31fd1a0
stfsy <s.pfaffel@gmail.com>
2020-04-12 10:45:20 +0200

    chore: use options to increment version accordingly

--- test/specs/incrementer.spec.js
+++ test/specs/incrementer.spec.js
@@ -5,28 +5,12 @@ const Incrementer = require('../../lib/incrementer')
 const expect = require('chai').expect
 const resolve = require('path').resolve
 
-const fileSystem = require('fs-promise')
-
 describe('Incrementer', () => {
 
     let incrementer = null
 
     beforeEach(() => {
-        incrementer = new Incrementer([resolve('test/test.package.json')])
-        return incrementer.readVersion()
-    })
-
-    afterEach(() => {
-        fileSystem.writeJsonSync(resolve('test/test.package.json'), { version: '1.2.3' })
-        fileSystem.writeJsonSync(resolve('test/test.package.withBuildMeta.json'), { version: '1.4.15+beta14-ABC' })
-    })
-
-    describe('.readVersion', () => {
-        it('should read the version from a given target .json file', (done) => {
-            incrementer.readVersion().then((version) => {
-                expect(version.toString()).to.equal('1.2.3')
-            }).then(done, done)
-        })
+        incrementer = new Incrementer([resolve('test/test.package.json')], { major: true })
     })
 
     describe('.handles', () => {
@@ -42,8 +26,8 @@ describe('Incrementer', () => {
 
     describe('incrementVersion', () => {
         it('should increment version and return stringified json', () => {
-            const json = incrementer.incrementVersion('{"version": "123"}')
-            expect(JSON.parse(json).version).to.equal('1.2.3')
+            const json = incrementer.incrementVersion('{"version": "1.2.3"}')
+            expect(JSON.parse(json).version).to.equal('2.0.0')
         })
     })
 })
