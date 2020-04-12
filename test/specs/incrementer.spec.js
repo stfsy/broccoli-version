d11e2744196cd037c6deae752f324fa6f7974d1a
stfsy <s.pfaffel@gmail.com>
2020-04-12 10:14:11 +0200

    refactor: handle version changes in memory

--- test/specs/incrementer.spec.js
+++ test/specs/incrementer.spec.js
@@ -12,54 +12,38 @@ describe('Incrementer', () => {
     let incrementer = null
 
     beforeEach(() => {
-
         incrementer = new Incrementer([resolve('test/test.package.json')])
+        return incrementer.readVersion()
     })
 
     afterEach(() => {
-
         fileSystem.writeJsonSync(resolve('test/test.package.json'), { version: '1.2.3' })
         fileSystem.writeJsonSync(resolve('test/test.package.withBuildMeta.json'), { version: '1.4.15+beta14-ABC' })
     })
 
-    it('should read the version from a given target .json file', (done) => {
-
-        incrementer.readVersion().then((version) => {
-
-            expect(version.toString()).to.equal('1.2.3')
-
-        }).then(done, done)
+    describe('.readVersion', () => {
+        it('should read the version from a given target .json file', (done) => {
+            incrementer.readVersion().then((version) => {
+                expect(version.toString()).to.equal('1.2.3')
+            }).then(done, done)
+        })
     })
 
-    it('should persist an incremented version number', (done) => {
-
-        incrementer.readVersion().then((version) => {
-
-            version.major(true)
-
-        }).then(incrementer.persistAll.bind(incrementer))
-            .then(incrementer.readVersion.bind(incrementer))
-            .then((version) => {
-
-                expect(version.toString()).to.equal('2.0.0')
-
-            }).then(done, done)
+    describe('.handles', () => {
+        it('should return true if holder file is passed', () => {
+            const handles = incrementer.handles([resolve('test/test.package.json')])
+            expect(handles).to.be.true
+        })
+        it('should return true if holder path does not match', () => {
+            const handles = incrementer.handles([resolve('test/test.package.json-suffix123')])
+            expect(handles).to.be.false
+        })
     })
 
-    it('should persist an incremented version number without build number if no build function was given', (done) => {
-
-        incrementer = new Incrementer([resolve('test/test.package.withBuildMeta.json')])
-
-        incrementer.readVersion().then((version) => {
-
-            version.major(true).meta()
-
-        }).then(incrementer.persistAll.bind(incrementer))
-            .then(incrementer.readVersion.bind(incrementer))
-            .then((version) => {
-
-                expect(version.toString()).to.equal('2.0.0')
-
-            }).then(done, done)
+    describe('incrementVersion', () => {
+        it('should increment version and return stringified json', () => {
+            const json = incrementer.incrementVersion('{"version": "123"}')
+            expect(JSON.parse(json).version).to.equal('1.2.3')
+        })
     })
 })
